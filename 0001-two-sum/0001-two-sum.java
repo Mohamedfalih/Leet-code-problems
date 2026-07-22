4aa73c23a72a953462e3d6c5a9143e5590d42f49
Mohamed Falih M <falih3261@gmail.com>
2026-07-22 16:02:37 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,3 +1,4 @@
+//O(n^2)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
@@ -11,4 +12,23 @@ class Solution {
         }
         return new int[]{};
     }
+}
+
+//O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0 ;i<nums.length;i++){
+            map.put(nums[i],i);
+        }
+
+        for(int i=0;i<nums.length;i++){
+            int find = target-nums[i];
+            if(map.containsKey(find) && map.get(find) != i){
+                return new int[]{i,map.get(find)};
+            }
+        }
+        return new int[]{};
+     }
 }
