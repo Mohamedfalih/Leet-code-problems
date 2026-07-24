8db5996158876178b41280d9ccddb91b7b9f82f7
Mohamed Falih M <falih3261@gmail.com>
2026-07-24 21:10:05 +0530

    Time: 2 ms (73.32%), Space: 42.2 MB (99.48%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+        int sp = 0;
+        int tp = 0;
+
+        while(sp < s.length() && tp < t.length()){
+
+            if(s.charAt(sp) == t.charAt(tp)){
+                sp++;
+                tp++;
+            }
+            else{
+                tp++;
+            }
+        }
+        
+        return sp == s.length() ? true : false; 
+    }
+}
