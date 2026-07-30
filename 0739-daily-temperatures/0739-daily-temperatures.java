9c3992890b5096ddb7cb9b3e9085ee0fbcdfd449
Mohamed Falih M <falih3261@gmail.com>
2026-07-30 14:19:57 +0530

    Update 0739-daily-temperatures.java

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -4,23 +4,16 @@ class Solution {
         Stack<Integer> st = new Stack<>();
         int[] res = new int[temp.length];
 
-        
-
-
         for(int i = temp.length-1 ; i >= 0 ; i--){
 
-            while(!st.isEmpty() && temp[st.peek()] <= temp[i]){
-
+            while(!st.isEmpty() && temp[st.peek()] <= temp[i])
                 st.pop();
-
-            }
 
             res[i] = st.isEmpty() ? 0 : st.peek() - i ;
 
             st.push(i);
             
         }
-        
         return res;
     }
 }
