59ddf82f4ed78c7b735d1ea387d07b6c12a90c71
Mohamed Falih M <falih3261@gmail.com>
2026-08-19 20:59:18 +0530

    Time: 1008 ms (5.01%), Space: 48.6 MB (98.19%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+
+        return atMost(nums , k) - atMost(nums , k-1);
+    }
+
+    static int atMost(int[] nums , int k){
+        int count = 0;
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        int start = 0;
+
+        for(int end = 0 ; end < nums.length ; end++){
+
+            map.put(nums[end] , map.getOrDefault(nums[end] , 0) + 1);
+
+            while(map.size() > k){
+                map.put(nums[start] , map.get(nums[start]) - 1);
+
+                if(map.get(nums[start]) == 0){
+                    map.remove(nums[start]);
+                }
+                start++;
+            }
+            
+            count += end - start + 1;
+
+            System.out.println(count);
+        }
+
+        return count;
+    }
+}
