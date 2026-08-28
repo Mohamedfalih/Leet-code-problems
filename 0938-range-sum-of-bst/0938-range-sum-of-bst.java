fe5db8630629b0c97dd3daa75d5ec834475ce067
Mohamed Falih M <falih3261@gmail.com>
2026-08-28 10:01:50 +0530

    Time: 0 ms (100%), Space: 54 MB (28.33%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -35,3 +35,20 @@ class Solution {
         return sum;
     }
 }
+
+// public int rangeSumBST(TreeNode root, int low, int high) {
+        
+//         if(root == null) return 0;
+
+        
+//         if(root.val > high){
+//             rangeSumBST(root.left , low , high);
+//         }
+//         if(root.val < low){
+//             rangeSumBST(root.right , low , high);
+//         }
+
+//         return root.val 
+//                     + rangeSumBST(root.left , low , high) 
+//                     + rangeSumBST(root.right , low , high);
+//     }
