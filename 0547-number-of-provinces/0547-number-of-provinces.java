84feb66e022bd2d5cfbae64fff1483b4d5374646
Mohamed Falih M <falih3261@gmail.com>
2026-08-29 15:44:15 +0530

    Time: 0 ms (100%), Space: 46.9 MB (97.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,41 +1,25 @@
 class Solution {
 
-    static void dfs(int node , boolean[] visited , HashMap<Integer , List<Integer>> adj) {
+    static void dfs(int node , boolean[] visited , int[][] adj) {
 		
 		visited[node]  = true;
 		
-		for(int nei : adj.get(node)) {
-			
-			if(!visited[nei]) {
-				dfs(nei , visited , adj);
+		for(int j = 0 ; j < adj.length ; j++) {
+			if(!visited[j] && adj[node][j] == 1) {
+				dfs(j , visited , adj);
 			}
 		}
 		
 	}
     public int findCircleNum(int[][] isConnected) {
-        
-        HashMap<Integer , List<Integer>> adj = new HashMap<>();
-
-        for(int i = 1 ; i <= isConnected.length ; i++){
-            adj.put(i , new ArrayList<>());
-        }
-
+    
+        boolean[] visited = new boolean[isConnected.length];
         int count = 0;
 
         for(int i = 0 ; i < isConnected.length ; i++){
-            for(int j = 0 ; j < isConnected.length ; j++){
-
-                if(isConnected[i][j] == 1 && i != j)
-                    adj.get(i+1).add(j+1);
-            }
-        }
-
-        boolean[] visited = new boolean[isConnected.length+1];
-
-        for(int i = 1 ; i <= isConnected.length ; i++){
             if(!visited[i]){
-                dfs(i , visited , adj);
                 count++;
+                dfs(i , visited , isConnected);
             }
         }
         return count;
